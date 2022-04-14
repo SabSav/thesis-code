c777ed7592e91c82dfcde0e80218cd895acbd705
Roman Belousov <roman.belousov@embl.de>
2022-04-15 00:10:37 +0200

    Chain and dynamic chain classes added with a metropolis pass function

--- code/ising.py
+++ code/ising.py
@@ -1,4 +1,4 @@
-"""Simulation of Ising model"""
+"""Simulation tools for Ising model"""
 
 import numpy as np
 
@@ -9,31 +9,88 @@ class Chain:
         spins (numpy.ndarray): Chain of up (+1) and down (-1) spin values
     """
 
-    def __init__(self, coupling = 1, field = 0., **kwargs):
+    def __init__(self, coupling=1, temperature=1.0, field=0., **kwargs):
         """Initialize an Ising chain
 
         Args:
-            coupling (int): Spin coupling-interaction constant is usually 1
-                (default) or -1.
+            coupling (int): Spin coupling-interaction constant is usually `1`
+                (default) or `-1`.
+            temperature (float): Temperature in Boltzmann-contant units
             field (float): External magnetic field
-            size (int): Chain size must be greater than or equal to 3 (default).
-                This keyword argument is ignored when the keyword argument
-                `spins` is given.
-            spins (None | array_like): When `None` the chain of size `size` is
+            size (int): Chain size must be greater than or equal to `3`
+                (default). This keyword argument is ignored when the keyword
+                argument `spins` is given.
+            spins (array_like): When unspecified the chain of size `size` is
                 initialized with randomly assigned spin values. Otherwise the
                 chain size and the initial spin values are taken from `spins`.
+
+        Raises:
+            TypeError: When unknown argument is provided
         """
         self.coupling = coupling
-        self.field = 0
+        self.temperature = temperature
+        self.field = field
         if 'spins' in kwargs:
             self.spins = np.asarray(kwargs['spins'])
+            del kwargs['spins']
         else:
-            size = kwargs['size'] if 'size' in kwargs else 3
+            size = 3
+            if 'size' in kwargs:
+                size = kwargs['size']
+                del kwargs['size']
             self.spins = 2 * np.random.randint(2, size=size) - 1
         assert len(self.spins) >= 3
+
+        if len(kwargs) > 0:
+            raise TypeError(f'Unknown kwargs: {list(kwargs.keys())}')
 
     def deltaE(self, i):
         """Return energy cost of flipping a given spin"""
         return 2 * self.field * self.spins[i] + self.coupling * self.spins[i] * (
             self.spins[i - 1] + self.spins[(i + 1) % len(self.spins)]
         )
+
+class DynamicChain(Chain):
+    """An extension of the Ising chain for stochastic simulations"""
+
+    def __init__(self, coupling=1, temperature=1.0, field=0., **kwargs):
+        """Overriden constructor with additional arguments
+
+        Args:
+            ...
+
+            action_rates (array_like): An array of shape `(N, 2)` for `N` spin
+                values `-1` and `1`
+        """
+        action_rates = (
+            kwargs.pop('action_rates') if 'action_rates' in kwargs
+            else None
+        )
+        super().__init__(coupling, temperature, field, **kwargs)
+
+        self.action_rates = (
+            np.full((len(self.spins), 2), 1.0) if action_rates is None
+            else np.asarray(action_rates)
+        )
+        assert self.action_rates.shape == (len(self.spins), 2)
+
+    def action_rate(self, i, value):
+        """Return action rate for a given value of an `i`th spin"""
+        return self.action_rates[i, (value + 1) // 2]
+
+def metropolis_pass(chain: Chain):
+    """Apply Metropolis step to each spin of the chain in random order
+
+    Returns
+        Chain: Updated chain
+    """
+
+    # Iterate over spin indices taken in the random order
+    for spin_to_change in np.random.permutation(np.arange(len(chain.config))):
+        dE = chain.deltaE(spin_to_change)
+
+        # Metropolis condition min(1, exp(...)) always holds for dE < 0
+        if np.random.random() < np.exp(- dE / chain.temperature):
+            chain.spins[spin_to_change] *= -1
+
+    return chain
--- test/test_ising.py
+++ test/test_ising.py
@@ -7,6 +7,7 @@ def test_Chain():
     """Test basic functionality of `ising.Chain`"""
     assert len(Chain(spins = [1, -1, 1, 1]).spins) == 4
     assert len(Chain(size=5).spins) == 5
+    raises(TypeError, Chain, uknown_keyword=True)
 
     # For 3 spins we have just two energy levels which differ by 2 J
     chain = Chain(coupling=3, spins = [1, 1, 1]);
@@ -17,3 +18,10 @@ def test_Chain():
     chain.spins[1] *= -1
     assert chain.deltaE(1) == -6
     assert chain.deltaE(0) == 0
+
+def test_DynamicChain():
+    """Test basic functionality of `ising.DynamicChain`"""
+    chain = DynamicChain()
+    assert (chain.action_rates == [[1.0, 1.0], [1.0, 1.0], [1.0, 1.0]]).all()
+    assert chain.action_rate(1, 1) == 1.0
+    assert chain.action_rate(1, -1) == 1.0
