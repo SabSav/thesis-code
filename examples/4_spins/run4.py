128e03fe8beec333beff98046ebbb23f1b2f5a5d
Roman Belousov <roman.belousov@embl.de>
2022-05-25 23:52:07 +0200

    4-spins adjusted

--- examples/4_spins/run4.py
+++ examples/4_spins/run4.py
@@ -13,25 +13,28 @@ size = 4
 lT = 0.5
 hT = 10
 h = 0
-J = -1
+J = 1
 # action_rates = np.empty((size, 2))
 # action_rates[:, 0] = 0.7
 # action_rates[:, 1] = 0.1
 # action_rates = np.array([[0.7, 0.1] for i in range(size)])
 
+# action_rates = np.array([
+#     2 * [0.7 if i % 2 == 0 else 0.1]
+#     for i in range(size)
+# ])
 action_rates = np.array([
-    2 * [0.7 if i % 2 == 0 else 0.1]
-    for i in range(size)
+    2 * [0.1] for i in range(size)
 ])
 
-dt_lT = 0.05
-dt_hT = 0.05
+dt_lT = 0.1
+dt_hT = 0.1
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 cases = [
     {'method': mc, 'label': 'Low-temperature sample from an MC simulation',
         'size': size, 'temperature': lT, 'field': h, 'coupling': J,
-        'burn_in': 10000, 'length': 1000000, 'frame_step': 100,
+        'burn_in': 10000, 'length': 1000000, 'frame_step': 10,
         'output': f'{dir_path}/mc-lT.json'},
     {'method': mc, 'label': 'High-temperature sample from an MC simulation',
         'size': size, 'temperature': hT, 'field': h, 'coupling': J,
@@ -41,7 +44,7 @@ cases = [
     {'method': alg1, 'label': 'Low-temperature sample from an A1 simulation',
         'size': size, 'temperature': lT, 'field': h, 'coupling': J,
         'action_rates': action_rates, 'dt': dt_lT,
-        'burn_in': 10000, 'length': 1000000, 'frame_step': 100,
+        'burn_in': 10000, 'length': 10000000, 'frame_step': 100,
         'output': f'{dir_path}/a1-lT.json'
     },
     {'method': alg1, 'label': 'High-temperature sample from an A1 simulation',
